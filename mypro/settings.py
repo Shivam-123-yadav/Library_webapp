900aafdcd6bcac1820cb66c0b73e67133f8a5d69
Shivam-123-yadav <141988688+Shivam-123-yadav@users.noreply.github.com>
2025-09-02 17:38:03 +0530

    Update settings.py

--- mypro/settings.py
+++ mypro/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = 'django-insecure-&$32zp$z8sbj#t%cxopoiunbztirx97xw@j-13)xdipxi8tz%d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # temporary for deployment
 
 
 # Application definition
