2354bf05bc32a11af36f72086a9e76211fa14166
Shivam-123-yadav <141988688+Shivam-123-yadav@users.noreply.github.com>
2025-09-02 18:38:53 +0530

    Update settings.py

--- mypro/settings.py
+++ mypro/settings.py
@@ -111,15 +111,14 @@ WSGI_APPLICATION = 'mypro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-       "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),       # Database name from Render
+        'USER': os.getenv('DB_USER'),       # Username from Render
+        'PASSWORD': os.getenv('DB_PASSWORD'), # Password from Render
+        'HOST': os.getenv('DB_HOST'),       # Hostname from Render
+        'PORT': os.getenv('DB_PORT', '5432'), # Port from Render
     }
 }
-
 
 
 # Password validation
